ef8eab8e4a8a0d0cd0876a9c4b9233b78c19f3f7
levent <levent1ozgur+github@protonmail.com>
2023-05-11 17:34:03 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-		// function to copy text to clipboard
+		// function to copy crypto addresses to clipboard
 		function copyToClipboard(text) {
 			var dummy = document.createElement("textarea");
 			document.body.appendChild(dummy);
@@ -10,3 +10,13 @@
 			var message = "The address has been copied to your clipboard: " + text;
 			alert(message);
 		}
+		// function to pop-up generated script code
+const generateBtn = document.querySelector('.custom-btn');
+
+generateBtn.addEventListener('click', () => {
+  const apps = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(app => app.value);
+  const message = `Set-ExecutionPolicy Bypass -Scope Process -Force;\n[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;\niex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));\nchoco install ${apps.join(' ')} -y;`;
+  alert(message);
+});
+
+
