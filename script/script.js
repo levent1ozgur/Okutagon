61061a9e7af6fc012fe46fe53312699a0b5cdcdc
levent <levent1ozgur+github@protonmail.com>
2023-05-18 21:15:37 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,16 +1,4 @@
-		// function to copy crypto addresses to clipboard
-		function copyToClipboard(text) {
-			var dummy = document.createElement("textarea");
-			document.body.appendChild(dummy);
-			dummy.value = text;
-			dummy.select();
-			document.execCommand("copy");
-			document.body.removeChild(dummy);
-			// show message to inform user that address was copied
-			var message = "the address has been copied to your clipboard: " + text;
-			alert(message);
-		}
-		// function to generated script code for win
+// function to generated script code for win
 document.addEventListener("DOMContentLoaded", function() {
   const generateButton = document.querySelector(".custom-btn.win");
 
