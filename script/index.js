e4c6d414e3b10a8d42838f04c2f773e39deb61e2
ㅤ <levent1ozgur+github@protonmail.com>
2024-05-11 20:31:52 +0300

    Create index.js

---
+++ script/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Check if JavaScript is enabled
+  const jsEnabled = document.getElementById("js-enabled");
+  jsEnabled.style.display = "block"; // Show the message
+
+  // Hide the message if JavaScript is enabled
+  jsEnabled.style.display = "none";
+});
