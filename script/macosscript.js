a60d015816d7e7e5a490d7cc72c591127ef49088
ㅤ <levent1ozgur+github@protonmail.com>
2024-05-11 20:18:57 +0300

    Create macosscript.js

---
+++ script/macosscript.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const selectedAppsContainer = document.getElementById(
+    "selected-apps-container"
+  );
+
+  // Function to generate script code for Windows
+  const generateWindowsScript = () => {
+    // Get all the selected checkboxes
+    const selectedCheckboxes = document.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+
+    // If no checkboxes are selected, clear the textarea and return
+    if (selectedCheckboxes.length === 0) {
+      selectedAppsContainer.innerHTML = ""; // Clear the textarea
+      return;
+    }
+
+    // Create an array to store the names of the selected apps
+    const selectedApps = [];
+
+    // Loop through the selected checkboxes and push their values to the array
+    selectedCheckboxes.forEach(function (checkbox) {
+      selectedApps.push(checkbox.value);
+    });
+
+    // Construct the string with the selected apps
+    const selectedAppsString = selectedApps.join(" ");
+
+    // Construct the final command string
+    const commandString = `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask ${selectedAppsString}`;
+
+    // Create a textarea element
+    const textarea = document.createElement("textarea");
+    textarea.rows = 10;
+    textarea.cols = 50;
+    textarea.value = commandString;
+    // Adjust the size of textarea based on its content
+    textarea.style.width = "100%";
+    textarea.style.height = "auto";
+    // Set textarea to be readonly
+    textarea.setAttribute("readonly", true);
+    // Disable textarea resizing
+    textarea.style.resize = "none";
+
+    // Clear the container and append the textarea
+    selectedAppsContainer.innerHTML = "";
+    selectedAppsContainer.appendChild(textarea);
+  };
+
+  // Add event listeners to checkboxes
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", generateWindowsScript);
+  });
+});
