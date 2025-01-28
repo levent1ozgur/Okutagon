23b268f4576e4f13210128f484eec2b8cde00b47
ㅤ <levent1ozgur+github@protonmail.com>
2025-01-28 18:25:02 +0300

    Update supporters.js

--- script/supporters.js
+++ script/supporters.js
@@ -1,7 +1,7 @@
 // Okutagon by Levent Özgür is licensed under CC BY-NC-SA 4.0 
 
   // URL of your Cloudflare Worker
-    const workerUrl = 'https://patreon-supporters.okutagon.workers.dev/';
+    const workerUrl = 'CF_WORKER_URL';
 
     // Fetch the supporter data from the Cloudflare Worker
     fetch(workerUrl)
